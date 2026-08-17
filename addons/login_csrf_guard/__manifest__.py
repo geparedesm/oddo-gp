4b24e0902ee8f4933e81f7be24a7718412bb7ec8
HGP <h@gp-developer.com>
2026-08-17 02:23:18 +0000

    [FIX] Prevent duplicate login submissions

---
+++ addons/login_csrf_guard/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Login CSRF Guard",
+    "version": "16.0.1.0.0",
+    "category": "Security",
+    "summary": "Prevent duplicate Odoo login submissions",
+    "depends": ["web"],
+    "data": ["views/web_login_templates.xml"],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
